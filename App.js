9be5b7a8c7852c4862b125a28552ff950fa38ec9
GonzaloVolonterio <volonterio.gonzalo1987@gmail.com>
2021-09-08 00:38:08 -0300

    up

--- App.js
+++ App.js
@@ -1,14 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, ImageBackground } from 'react-native';
+import * as Speech from "expo-speech"
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+
+  state = {
+    inputWords: ""
+  }
+
+  speak() {
+    //const words = "Hello everyone"
+    //Speech.speak(words)
+    const words = this.state.inputWords
+    Speech.speak(words)
+  }
+
+  render() {
+    return (
+      < View style={styles.container} >
+        <ImageBackground source={require('./assets/fondo01.png')} style={styles.backgroungImage}>
+          <TextInput
+            placeholder="Write some words"
+            onChangeText={text => this.setState({ inputWords: text })}
+          />
+
+          <Button color="#841584"
+            title="Say Something"
+            onPress={() => { this.speak() }}
+          />
+        </ImageBackground>
+      </View >
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -17,5 +41,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  backgroungImage: {
+    resizeMode: "cover",
+    width: "100%",
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    opacity: 0.6
   },
 });
